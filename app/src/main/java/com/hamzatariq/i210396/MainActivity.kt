e4ce08693f1446249d6a3cf567cc56d0aec8eb7f
“Hamzatariq211” <hamzatariq20@outlook.com>
2025-09-02 17:52:37 +0500

    splash and login screens

--- app/src/main/java/com/hamzatariq/i210396/MainActivity.kt
+++ app/src/main/java/com/hamzatariq/i210396/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.hamzatariq.i210396
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Move to LoginScreen after 2 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, loginscreen::class.java)
+            startActivity(intent)
+            finish() // Optional: prevents back navigation to splash
+        }, 2000) // 2000ms = 2 seconds
     }
 }
